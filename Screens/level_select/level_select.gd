a1ecbe7b529db76480bc49e40366e02fb37a2f68
DinoStarBros <lmatthewalven@gmail.com>
2026-03-12 23:52:41 +0800

    Add level select buttons and handlers
    Wire up the level select screen: add a level_scns array and a button_and_level dictionary in level_select.gd, connect each button's pressed signal to its handler (L1 loads the first scene via SceneManager.change_scene, L2/L3 use placeholder prints). Update the .tscn to mark l1/l2/l3 with unique_name_in_owner and add the new l3 button node. This enables basic level selection wiring for further implementation.

--- Screens/level_select/level_select.gd
+++ Screens/level_select/level_select.gd
@@ -1,1 +1,23 @@
 extends Control
+
+const level_scns : Array[String] = [
+	"res://Screens/prototype_1/combo_knight_like_proto.tscn",
+	
+]
+
+@onready var button_and_level : Dictionary = {
+	"L1": [
+		%l1, func():SceneManager.change_scene(level_scns[0])
+		],
+	"L2": [
+		%l2, func():print("2")
+		],
+	"L3": [
+		%l3, func():print("3")
+		],
+	
+}
+
+func _ready() -> void:
+	for n in button_and_level.values(): if n[0] is Button:
+		n[0].pressed.connect(n[1])
