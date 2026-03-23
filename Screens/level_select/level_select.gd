f5d6bf5e9e0f02f4bce7abc12735d72e3ded7e40
DinoStarBros <lmatthewalven@gmail.com>
2026-03-23 21:54:44 +0800

    Add References and move prototype_1 to level_1
    Introduce a new GlobalScripts/References.gd and register it in project.godot. Rename/move prototype_1 resources into Screens/level_1 (script, scene, and supporting line script), update the level scene's script reference and root node name. Update level_select.gd to point to the Level1 scene by UID and adjust button callbacks, and fix belt.tscn to reference the moved line.gd. These changes reorganize the prototype into a proper Level1 namespace and add a centralized References node.

--- Screens/level_select/level_select.gd
+++ Screens/level_select/level_select.gd
@@ -1,19 +1,22 @@
 extends Control
 
 const level_scns : Array[String] = [
-	"res://Screens/prototype_1/combo_knight_like_proto.tscn",
+	 ## Level 1
 	
 ]
 
 @onready var button_and_level : Dictionary = {
 	"L1": [
-		%l1, func():SceneManager.change_scene(level_scns[0])
+		%l1, 
+		func():SceneManager.change_scene("uid://blemjwhstvb1w")
 		],
 	"L2": [
-		%l2, func():print("2")
+		%l2, 
+		func():print("2")
 		],
 	"L3": [
-		%l3, func():print("3")
+		%l3, 
+		func():print("3")
 		],
 	
 }
