b16079fd6420da2daf05b023ea0336590472ac32
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-04-29 16:45:59 +0800

    Whar

--- entities/EnemyClass.gd
+++ entities/EnemyClass.gd
@@ -1,10 +1,12 @@
 extends Node2D
 ## Abstract class for enemies 
-class_name  Enemy
+class_name Enemy
 
 const HEALTHBAR_POSITION : Vector2 = Vector2(880, 138)
 
 var stat_headstart : float = 0
+
+
 
 func _ready() -> void:
 	pass
